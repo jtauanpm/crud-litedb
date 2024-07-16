695b62b2ed400a7db66db96f905d0dc3729410a8
jtauanpm <jtauan10@gmail.com>
2024-07-16 19:37:47 -0300

    upgrade

--- crud-litedb.domain/Services/IUserInputService.cs
+++ crud-litedb.domain/Services/IUserInputService.cs
@@ -3,10 +3,11 @@ using crud_litedb.Models;
 namespace crud_litedb.Services;
 
 public interface IUserInputService
-{ 
+{   
+
+
+    
     Person? FillPerson(int id = 0);
     int? GetId();
     void FlowPause();
-
-    
 }
